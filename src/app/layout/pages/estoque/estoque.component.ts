0470816af5a14b9377efdf9fd96674937e63f9e5
Debora Nogueira <deboran.silva@hotmail.com>
2025-02-11 16:29:46 -0300

    Finalizando o projeto - últimas alterações e ajustes.

---
+++ src/app/layout/pages/estoque/estoque.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-estoque',
+  imports: [],
+  templateUrl: './estoque.component.html',
+  styleUrl: './estoque.component.scss'
+})
+export class EstoqueComponent {
+
+}
